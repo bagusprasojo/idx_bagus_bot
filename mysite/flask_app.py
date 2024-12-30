6ce6dfb1954e9d2ebf3f49dd1a977aa83e82cc0f
bagus ryzen <bagusprasojo@tigaserangkai.co.id>
2024-12-30 11:07:21 +0700

    nambah website

--- mysite/flask_app.py
+++ mysite/flask_app.py
@@ -1,11 +1,26 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello from Flask! Yes, Flask Not Flash !'
+def homepage():
+    return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/daftar_pengakses')
+def daftar_pengakses():
+    return render_template('daftar_pengakses.html')
+
+@app.route('/daftar_berita')
+def daftar_berita():
+    return render_template('daftar_berita.html')
+
+@app.route('/keterbukaan_informasi')
+def keterbukaan_informasi():
+    return render_template('keterbukaan_informasi.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
